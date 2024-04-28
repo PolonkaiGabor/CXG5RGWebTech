f833551bb5b0a5d5396e3d1d9cc468135940f22d
PolonkaiGabor <112873585+PolonkaiGabor@users.noreply.github.com>
2024-04-29 00:40:15 +0200

    feltoltes

---
+++ CXG5RG_0318/feldolgozo.php
@@ -0,0 +1,22 @@
+<html lang="hu">
+    <head>
+        <meta charset="UTF-8">
+        <title>asd</title>
+    </head>
+    <body>
+        <font style="font-size:18pt">
+        <h2>HTML5 űrlap_CXG5RG</h2>
+
+        <?php
+        echo ("<strong>Név: <strong>" . $_POST['nev'] . "<br><br>");
+        echo ("<strong>PIN kód: <strong>" . $_POST['kod'] . "<br><br>");
+        echo ("<strong>Gyümölcs: <strong>" . $_POST['gyumolcs'] . "<br><br>");
+        echo ("<strong>Életkor: <strong>" . $_POST['eletkor'] . "<br><br>");
+        echo ("<strong>Lábméret: <strong>" . $_POST['labmeret'] . "<br><br>");
+        echo ("<strong>Önbizalom: <strong>" . $_POST['onbizalom'] . "<br><br>");
+        ?>
+
+        <a href="CXG5RG_uralp.hmtl">Vissza az űrlapra</a>
+</font>
+    </body>
+</html>
