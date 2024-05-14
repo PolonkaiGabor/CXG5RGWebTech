39cac73b859139600b931233c2654d9bea3f8edd
PolonkaiGabor <112873585+PolonkaiGabor@users.noreply.github.com>
2024-05-14 03:08:10 +0200

    potlas2

--- CXG5RG_0506/CXG5RG_3.js
+++ CXG5RG_0506/CXG5RG_3.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
-    // Doboz eltávolítása gomb eseménykezelője
     $('#eltavolitas').click(function() {
-        $('#doboz').hide(); // Doboz elrejtése
+        $('#doboz').hide();
     });
 
-    // DIV elemek kiürítése gomb eseménykezelője
     $('#kiurites').click(function() {
-        $('#doboz').empty(); // Doboz tartalmának kiürítése
+        $('#doboz').empty();
     });
 });
--- CXG5RG_0506/CXG5RG_4.js
+++ CXG5RG_0506/CXG5RG_4.js
@@ -1,23 +1,19 @@
 $(document).ready(function() {
-    // Húzható elem inicializálása
     $("#draggable").draggable();
 
-    // Elem megjelenítése gomb eseménykezelője
     $('#megjelenit').click(function() {
-        $('#draggable').show("fold", 1000); // Elem megjelenítése animációval
+        $('#draggable').show("fold", 1000);
     });
 
-    // Elem elrejtése gomb eseménykezelője
     $('#elrejtes').click(function() {
-        $('#draggable').hide("explode", 1000); // Elem elrejtése animációval
+        $('#draggable').hide("explode", 1000);
     });
 
-    // Elem animálása gomb eseménykezelője
     $('#animacio').click(function() {
         $('#draggable').animate({
             width: "200px",
             height: "200px",
             lineHeight: "200px"
-        }, 1000); // Elem animálása
+        }, 1000);
     });
 });
